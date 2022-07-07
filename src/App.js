f207fb4d6c3f22dd1aee35b5050ab53368abb46f
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-07-07 14:03:03 +0100

    Added Theming

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-
-import { useStateContext } from './contexts/ContextProvider';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import {
@@ -24,21 +22,41 @@ import {
   ColorMapping,
   Editor,
 } from './pages';
-
 import './App.css';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, [setCurrentColor, setCurrentMode]);
+
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
           <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
             <TooltipComponent content='Settings' position='Top'>
               <button
                 type='button'
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '50%' }}
                 className='text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray'
-                style={{ background: 'yellow', borderRadius: '50%' }}
               >
                 <FiSettings />
               </button>
@@ -63,8 +81,9 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full '>
               <Navbar />
             </div>
-
             <div>
+              {themeSettings && <ThemeSettings />}
+
               <Routes>
                 {/* dashboard  */}
                 <Route path='/' element={<Ecommerce />} />
@@ -92,6 +111,7 @@ const App = () => {
                 <Route path='/stacked' element={<Stacked />} />
               </Routes>
             </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
